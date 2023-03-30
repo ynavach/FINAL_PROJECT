e292eb5564d071f317d6860fbe372e1001af9d90
pulgazion <reimarrodri@gmail.com>
2023-03-30 15:08:25 +0000

    Nombres adicionales en la navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,52 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-expand-md navbar-light bg-light">
+		  <div className="container">
+			<Link to="/" className="navbar-brand">
+			  React Boilerplate
+			</Link>
+			<button
+			  className="navbar-toggler"
+			  type="button"
+			  data-toggle="collapse"
+			  data-target="#navbarNav"
+			  aria-controls="navbarNav"
+			  aria-expanded="false"
+			  aria-label="Toggle navigation"
+			>
+			  <span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+			  <ul className="navbar-nav mx-auto text-center">
+				<li className="nav-item">
+				<Link to="/" className="nav-link">
+         			 <span style={{ color: "fuchsia" }}>Inicio</span>
+        		</Link>
+				</li>
+				<li className="nav-item">
+				  <Link to="/about" className="nav-link">
+					Nosotros
+				  </Link>
+				</li>
+				<li className="nav-item">
+				  <Link to="/services" className="nav-link">
+					Servicios
+				  </Link>
+				</li>
+				<li className="nav-item">
+				  <Link to="/contact" className="nav-link">
+					Contacto
+				  </Link>
+				</li>
+			  </ul>
+			  <div className="ml-auto">
+				<Link to="/demo">
+				  <button className="btn btn-primary">Check the Context in action</button>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+			  </div>
 			</div>
+		  </div>
 		</nav>
-	);
+	  );
 };
