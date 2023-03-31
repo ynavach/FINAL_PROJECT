6ed5afc9a13d49b8602416469f8e1f9c9fbe2653
N1kf3 <wilfredo.andrade92@gmail.com>
2023-03-31 01:45:57 +0000

    cambios a la logica para validar la informacion v1

--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -12,32 +12,52 @@ export const Signup = () => {
 	const [userPhone, setUserPhone] = useState();
 
 
+	const verifyInput=(email,pass,name,last,phone)=>{
+		if ((email == undefined)||(pass== undefined)||(name== undefined)||(last== undefined)||(phone== undefined)){
+			console.log("undefined")
+			return false
+		} else {
+			console.log("definido")
+			if((email.trim().length >= 1) && (pass.trim().length >= 1) && (name.trim().length >= 1) && (last.trim().length >= 1)&&(phone.trim().length >= 1)){
+				return true
+			}
+			return false
+		}
+	}
+
 	const createUser= async()=>{
-		bodyUser= {
+		let bodyUser= {
 			"email": userEmail,
 			"password": userPass,
 			"name":userName,
 			"last_name":userLastName,
 			"phone_number":userPhone
 		}
-		try{
-			const response = await fetch('url',{
-				method: "POST",
-				headers:{
-					'Content-Type': 'application/json'
-				},
+		console.log(bodyUser)
+		if (verifyInput(userEmail,userPass,userName,userLastName,userPhone)){
+			console.log("carga datos")
+			try{
+				const response = await fetch('url',{
+					method: "POST",
+					headers:{
+						'Content-Type': 'application/json'
+					},
+	
+					body: JSON.stringify(bodyUser)
+				})
+			}
+			catch(error){
+				console.log(error)
+			}
+		}else{
+			console.log("rechaza datos")
+		}
 
-				body: JSON.stringify(bodyUser)
-			})
-		}
-		catch(error){
-			console.log(error)
-		}
 
 	}
 	return (
 		<div className="container mt-5">
-			<form className="col-4 p-5 border border-dark rounded-4">
+			<div className="col-4 p-5 border border-dark rounded-4">
 				<div className="d-flex justify-content-between"> 
 					<div className="mb-3">
 						<label htmlFor="userName" className="form-label" >Nombre</label>
@@ -61,7 +81,7 @@ export const Signup = () => {
 					<input type="password" className="form-control" id="exampleInputPassword1" onChange={e=>setUserPass(e.target.value)}/>
 				</div>
 				<button type="" className="btn btn-primary" onClick={e=>createUser()}>Crear</button>
-			</form>
+			</div>
 		</div>
 	);
 };
