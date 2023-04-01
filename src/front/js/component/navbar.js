4c5a2764c4fc463c664412391e277d0d35e40cf9
ynavach <yunensey@hotmail.com>
2023-04-01 14:45:04 +0000

    actualizando enlaces a las vistas de servicios y login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,15 +6,16 @@ export const Navbar = () => {
 	return(
 		<nav className="navbar navbar-expand-lg" style={{backgroundColor:"#AB46D2"}}>
   <div className="container-fluid">
-    <div> 
-      <i className= "fa-sharp fa-solid fa-paw text-light  fa-5x fa-lg b-2"></i>
-      <i className= "fa-sharp fa-solid fa-paw text-light  fa-2x t-4"></i>
-    </div>
-    <a className="navbar-brand ms-3" style={{color:"#FFFFFF"}} href="#" >  Huellas PetCare </a>
-    <div> 
-      <i className= "fa-sharp fa-solid fa-paw text-light  fa-5x fa-lg b-2"></i>
-      <i className= "fa-sharp fa-solid fa-paw text-light  fa-2x t-4"></i>
-    </div>
+    <Link to="/">
+        <div> 
+          <i className= "fa-sharp fa-solid fa-paw text-light  fa-5x fa-lg b-2"></i>
+          <i className= "fa-sharp fa-solid fa-paw text-light  fa-2x t-4"></i>               
+          <span className="navbar-brand ms-3" style={{color:"#FFFFFF"}} href="#" >Huellas PetCare</span>
+          <i className= "fa-sharp fa-solid fa-paw text-light  fa-5x fa-lg b-2"></i>
+          <i className= "fa-sharp fa-solid fa-paw text-light  fa-2x t-4"></i>
+        </div>
+    </Link>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -24,16 +25,20 @@ export const Navbar = () => {
           <a className="nav-link active" aria-current="page" style={{color:"#FFFFFF"}} href="#">Quienes Somos</a>
         </li>
         <li className="nav-item" >
-          <a className="nav-link" style={{color:"#FFFFFF"}} href="#">Servicios</a>
+        <Link to="/services">
+          <span className="nav-link" style={{color:"#FFFFFF"}} href="#">Servicios</span>
+        </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" style={{color:"#FFFFFF"}} href="#">Contáctanos</a>
+          <span className="nav-link" style={{color:"#FFFFFF"}} href="#">Contáctanos</span>
+        </li>
+        <li className="nav-item">        
+          <span className="nav-link" style={{color:"#FFFFFF"}} href="#">Regístrate</span>
         </li>
         <li className="nav-item">
-          <a className="nav-link" style={{color:"#FFFFFF"}} href="#">Regístrate</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" style={{color:"#FFFFFF"}} href="#">Acceso</a>
+        <Link to="/login">
+          <span className="nav-link" style={{color:"#FFFFFF"}} href="#">Acceso</span>
+        </Link>  
         </li>
       </ul>
     </div>
