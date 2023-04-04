03e6a53ce9624b33dd484d656d4e162a961c6b76
N1kf3 <wilfredo.andrade92@gmail.com>
2023-04-04 16:32:04 +0000

    cambios en el signup

--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import tiye from "../../img/tj.jpg";
-
 import { Context } from "../store/appContext";
 
 export const Signup = () => {
@@ -15,22 +14,10 @@ export const Signup = () => {
   const navigate = useNavigate();
 
   const verifyInput = (email, pass, name, last, phone) => {
-    if (
-      email == undefined ||
-      pass == undefined ||
-      name == undefined ||
-      last == undefined ||
-      phone == undefined
-    ) {
+    if (email == undefined || pass == undefined || name == undefined ||  last == undefined || phone == undefined) {
       return false;
     } else {
-      if (
-        email.trim().length >= 1 &&
-        pass.trim().length >= 1 &&
-        name.trim().length >= 1 &&
-        last.trim().length >= 1 &&
-        phone.trim().length >= 1
-      ) {
+      if (email.trim().length >= 1 && pass.trim().length >= 1 && name.trim().length >= 1 && last.trim().length >= 1 && phone.trim().length >= 1) {
         return true;
       }
       return false;
@@ -75,7 +62,7 @@ export const Signup = () => {
     }
   };
   return (
-    <div>
+    <div className="signup-flow" style={{minHeight:"99vh"}}>
       <div className="container d-flex align-items-center justify-content-center signup-css">
         <form className="col-4 p-5 bg-light  signup-form">
           <div className="d-flex flex-column align-items-center">
@@ -86,76 +73,41 @@ export const Signup = () => {
               <label htmlFor="userName" className="form-label">
                 Nombre
               </label>
-              <input
-                type="text"
-                className="form-control"
-                id="userName"
-                onChange={(e) => setUserName(e.target.value)}
-              />
+              <input type="text" className="form-control" id="userName" onChange={(e) => setUserName(e.target.value)}/>
             </div>
             <div className="mb-3">
               <label htmlFor="userLastName" className="form-label">
                 Apellido
               </label>
-              <input
-                type="text"
-                className="form-control"
-                id="userLastName"
-                onChange={(e) => setUserLastName(e.target.value)}
-              />
+              <input type="text" className="form-control" id="userLastName" onChange={(e) => setUserLastName(e.target.value)}/>
             </div>
           </div>
           <div className="mb-3">
             <label htmlFor="userPhone" className="form-label">
               Número de teléfono
             </label>
-            <input
-              type="text"
-              className="form-control"
-              id="userPhone"
-              onChange={(e) => setUserPhone(e.target.value)}
-            />
+            <input type="text" className="form-control" id="userPhone" onChange={(e) => setUserPhone(e.target.value)}/>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Correo Electrónico
             </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              onChange={(e) => setUserEmail(e.target.value)}
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setUserEmail(e.target.value)}
             />
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">
               Contraseña
             </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              onChange={(e) => setUserPass(e.target.value)}
-            />
+            <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setUserPass(e.target.value)}/>
           </div>
           <div className="d-flex flex-column align-items-center">
-            <button
-              type=""
-              className="btn text-white"
-              style={{ backgroundColor: "#AB46D2" }}
-              onClick={createUser}
-            >
+            <button type="" className="btn text-white" style={{ backgroundColor: "#AB46D2" }} onClick={createUser}>
               Crear Cuenta
             </button>
           </div>
         </form>
-        <img
-          className=" image-form"
-          src={tiye}
-          alt="JWT Auth Logo"
-          style={{ objectFit: "contain" }}
-        />
+        <img className=" image-form" src={tiye} alt="JWT Auth Logo" style={{ objectFit: "contain" }}/>
       </div>
     </div>
   );
