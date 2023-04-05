a3f25cb2402e48f1f00f4ecf6c0fe930c33120c3
pulgazion <reimarrodri@gmail.com>
2023-04-05 20:49:31 +0000

    Cambios de imagen

--- src/front/js/component/gallery.jsx
+++ src/front/js/component/gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import fondogalvUrl from "../../img/fondogal.png";
+import { Container, Row, Col } from 'react-bootstrap';
+import fondogal2Url from "../../img/fondogal2.png";
 import clinica1Url from "../../img/clinica1.jpg";
 import clinica2Url from "../../img/clinica2.jpg";
 import clinica3Url from "../../img/clinica3.jpg";
@@ -9,18 +9,27 @@ import clinica7Url from "../../img/clinica7.jpg";
 import clinica8Url from "../../img/clinica8.png";
 
 const Gallery = () => {
+
   return (
-    <div style={{ position: "relative"}}>
-      <img
-        src={fondogalvUrl}
-        alt="Fondo de Galeria"
-      />
-      <div style={{ marginTop: "100px" }}>
-        <Container>
-          <h1 className="mb-5">Galeria</h1>
-          <div className="fusion-column col-lg-5 col-md-5 col-sm-5" style={{ marginTop: "20px", marginBottom: "50px" }}>
-            <p>¡Bienvenidos a nuestra galería de Huellas PetCare! En nuestra clínica veterinaria, nos enorgullece no solo brindar atención médica excepcional a nuestras mascotas, sino también crear un ambiente cálido y acogedor donde se sientan seguros y felices. Aquí, hemos recopilado algunas fotos de nuestras instalaciones, para que puedan ver por sí mismos la calidad de lo que brindamos. Estamos aquí para asegurarnos de que todas las mascotas que ingresan por nuestras puertas se sientan como en casa. ¡Disfruten de nuestra galería y esperamos verlos pronto en nuestra clínica!</p>
-          </div>
+    <div style={{ position: "relative" }}>
+      <Container style={{ marginTop: "100px" }}>
+            <Row>
+              <Col md={6}>
+                <h1 className="mb-5 mt-5">Galeria</h1>
+                <div className="fusion-column col-lg-12 col-md-12 col-sm-12" style={{ marginTop: "50px", marginBottom: "50px" }}>
+                  <p>
+                  Bienvenidos a nuestra galería de Huellas PetCare! En nuestra clínica veterinaria, nos enorgullece no solo brindar atención médica excepcional a nuestras mascotas, sino también crear un ambiente cálido y acogedor donde se sientan seguros y felices. Aquí, hemos recopilado algunas fotos de nuestras instalaciones, para que puedan ver por sí mismos la calidad de lo que brindamos. Estamos aquí para asegurarnos de que todas las mascotas que ingresan por nuestras puertas se sientan como en casa. ¡Disfruten de nuestra galería y esperamos verlos pronto en nuestra clínica!
+                  </p>
+                </div>
+              </Col>
+              <Col md={6}>
+                <img
+                src={fondogal2Url}
+                alt="Fondo de servicios"
+                style={{ width: "120%", position: "relative" }}
+              />
+              </Col>
+          </Row>
           <div className="row">
             <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
               <img
@@ -59,10 +68,11 @@ const Gallery = () => {
               />
             </div>
           </div>
-        </Container>
-      </div>
+      </Container>
     </div>
   );
 };
 
 export default Gallery;
+
+
