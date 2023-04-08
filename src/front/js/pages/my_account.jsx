8d9b50c77353fd53b6fce0555725b27820e3553a
Carlos Herrera <c.herrerav97@gmail.com>
2023-04-08 18:42:04 +0000

    Estilos Vista Info Perfil

--- src/front/js/pages/my_account.jsx
+++ src/front/js/pages/my_account.jsx
@@ -34,24 +34,31 @@ export const My_account=()=>{
     return (
         <div className="my-account-css">
             <div className="d-flex m-5 justify-content-between h-100 "> 
-                <div className="border border-dark col-3 d-flex flex-column me-5 fondo">
-                    <div className="flex-fill border-bottom text-center"> 
-                        <h5>Informacion del usuario:</h5>
+                <div className="border col-3 d-flex flex-column me-5 fondo">
+                    <div className="d-flex align-items-center justify-content-center flex-column"> 
+                        <h5 className="text-light pt-5">INFORMACIÓN DE USUARIO</h5>
                         {
                             store.user ? (
-                                    <div >
-                                        <div>Nombre y apellido: {store.user.name} {store.user.last_name}</div>
-                                        <div>email: {store.user.email}</div>
-                                        <div>Telefono: {store.user.phone_number}</div>
+                                    <div className="d-flex align-items-center justify-content-center flex-column p-3">
+                                        <img className="profile-img pb-2" src="https://cdn-icons-png.flaticon.com/512/3076/3076404.png" alt="IMG Profile"/>
+                                        <h6 className="text-light p-2">Nombre: {store.user.name} {store.user.last_name}</h6>
+                                        <h6 className="text-light p-2">Email: {store.user.email}</h6>
+                                        <h6 className="text-light p-2">Teléfono: {store.user.phone_number}</h6>
                                     </div>
-                            ) : (<h5>Cargando información privada...</h5>)
+                            ) : (
+                                <div className="d-flex align-items-center justify-content-center fs-3" style={{height:"45vh"}} >
+                                    <div className="spinner-border text-light" role="status">
+                                        <span className="sr-only">Loading...</span>
+                                    </div>
+                                </div>	
+                            )
                         }
                     </div>
-                    <div className="flex-fill border-bottom "> 
-                        <button  onClick={e=>LoadPage(1)}>MIS MASCOTAS </button> 
+                    <div className="d-flex align-items-center justify-content-center p-3"> 
+                        <button onClick={e=>LoadPage(1)} className="btn btn-light hover-effect" style={{color:"#AB46D2", fontWeight:"bold"}}>MIS MASCOTAS </button> 
                     </div>
-                    <div className="flex-fill">
-                        <button onClick={e=>LoadPage(2)}>SERVICIOS AGENDADOS</button>
+                    <div className="d-flex align-items-center justify-content-center p-3">
+                        <button onClick={e=>LoadPage(2)} className="btn btn-light hover-effect" style={{color:"#AB46D2", fontWeight:"bold"}}>SERVICIOS AGENDADOS</button>
                     </div>
                 </div>
                 
