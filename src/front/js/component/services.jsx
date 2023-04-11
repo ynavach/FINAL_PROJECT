9b3a9b6c03a83ee7fa112b06c7d2c31fc5bf9d8a
Carlos Herrera <c.herrerav97@gmail.com>
2023-04-11 07:29:08 +0000

    FrontEnd para Agendar Servicios

--- src/front/js/component/services.jsx
+++ src/front/js/component/services.jsx
@@ -23,6 +23,8 @@ const Services = () => {
     setHoveredCard(null);
   };
 
+  const name2 = "Prueba"
+
 
   return (
     <div style={{ position: "relative" }}>
@@ -57,7 +59,7 @@ const Services = () => {
           <Col md={4} xs={12} className="mb-4">
             <Card
               style={{
-                height: "106%",
+                height: "65vh",
                 boxShadow:
                 hoveredCard === 0 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
               }}
@@ -65,171 +67,193 @@ const Services = () => {
               onMouseLeave={handleCardLeave}
             >
               <Card.Body>
-                <img
-                  src={consultaUrl}
-                  style={{ display: "block", margin: "auto" }}
-                  width="120"
-                  height="150"
-                  alt="Foto de Consulta"
-                />
+                <img src={consultaUrl} style={{ display: "block", margin: "auto" }} width="150" height="150" alt="Foto de Consulta" />
                 <Card.Title>Consultas</Card.Title>
-              <Card.Text>
-                Ofrecemos consultas para perros, gatos y animales exóticos.
-              </Card.Text>
-              <Card.Text>Algunos de nuestros servicios:</Card.Text>
-              <ul>
-                <li>Consulta de rutina</li>
-                <li>Consulta de enfermedades</li>
-                <li>Consulta post-operación</li>
-              </ul>
-              {
-                store.jwt_token ?
-                  <Modal_Services name="Servicio de Consultas"/>
-                :
-                  <div className="d-flex align-items-center justify-content-center">
-                    <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
-                      Agendar Servicio
-                    </button>               
-                  </div>
-              }
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} xs={12}>
-          <Card
-            style={{
-              height: "100%",
-              boxShadow:
-              hoveredCard === 1 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
-            }}
-            onMouseEnter={() => handleCardHover(1)}
-            onMouseLeave={handleCardLeave}
-          >
-            <Card.Body>
-              <img
-                src={vacunacionUrl}
-                style={{ display: "block", margin: "auto", marginTop: "20px", marginBottom: "25px" }}
-                width="100"
-                height="100"
-                alt="Foto de Consulta"
-              />
-              <Card.Title>Vacunación</Card.Title>
-              <Card.Text>
-                Contamos con vacunas para prevenir diversas enfermedades.
-              </Card.Text>
-              <Card.Text>Algunos de nuestros servicios:</Card.Text>
-              <ul>
-                <li>Vacuna contra la rabia</li>
-                <li>Vacuna contra el parvovirus</li>
-                <li>Vacuna contra la leptospirosis</li>
-              </ul>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} xs={12}>
-          <Card
-            style={{
-              height: "100%",
-              boxShadow:
-              hoveredCard === 2 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
-            }}
-            onMouseEnter={() => handleCardHover(2)}
-            onMouseLeave={handleCardLeave}
-          >
-            <Card.Body>
-              <img
-                src={cirugiaUrl}
-                style={{ display: "block", margin: "auto" }}
-                width="120"
-                height="150"
-                alt="Foto de Consulta"
-              />
-              <Card.Title>Cirugía</Card.Title>
-              <Card.Text>Realizamos cirugías de diversos tipos.</Card.Text>
-              <Card.Text>Algunos de nuestros servicios:</Card.Text>
-              <ul>
-                <li>Esterilización</li>
-                <li>Cirugía de columna</li>
-                <li>Cirugía ortopédica</li>
-              </ul>
-            </Card.Body>
-          </Card>
-        </Col>
+                <Card.Text>
+                  Ofrecemos consultas para perros, gatos y animales exóticos.
+                </Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Consulta de rutina</li>
+                  <li>Consulta de enfermedades</li>
+                  <li>Consulta post-operación</li>
+                </ul>
+                {
+                  store.jwt_token ?
+                    <Modal_Services name="Servicio de Consultas"/>
+                  :
+                    <div className="d-flex align-items-center justify-content-center">
+                      <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
+                        Agendar Servicio
+                      </button>               
+                    </div>
+                }
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} xs={12}>
+            <Card
+              style={{
+                height: "65vh",
+                boxShadow:
+                hoveredCard === 1 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
+              }}
+              onMouseEnter={() => handleCardHover(1)}
+              onMouseLeave={handleCardLeave}
+            >
+              <Card.Body>
+                <img src={vacunacionUrl} style={{ display: "block", margin: "auto", marginTop:"3vh", marginBottom:"3vh"}} width="110" height="110" alt="Foto de Consulta" />
+                <Card.Title>Vacunación</Card.Title>
+                <Card.Text>
+                  Contamos con vacunas para prevenir diversas enfermedades.
+                </Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Vacuna contra la rabia</li>
+                  <li>Vacuna contra el parvovirus</li>
+                  <li>Vacuna contra la leptospirosis</li>
+                </ul>
+                {
+                  store.jwt_token ?
+                    <Modal_Services name="Servicio de Vacunación"/>
+                  :
+                    <div className="d-flex align-items-center justify-content-center">
+                      <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
+                        Agendar Servicio
+                      </button>               
+                    </div>
+                }
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} xs={12}>
+            <Card
+              style={{
+                height: "65vh",
+                boxShadow:
+                hoveredCard === 2 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
+              }}
+              onMouseEnter={() => handleCardHover(2)}
+              onMouseLeave={handleCardLeave}
+            >
+              <Card.Body>
+                <img src={cirugiaUrl} style={{ display: "block", margin: "auto" }} width="150" height="150" alt="Foto de Consulta"/>
+                <Card.Title>Cirugía</Card.Title>
+                <Card.Text>Realizamos cirugías de diversos tipos según las necesidades de tu mascota.</Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Esterilización</li>
+                  <li>Cirugía de columna</li>
+                  <li>Cirugía ortopédica</li>
+                </ul>
+                {
+                  store.jwt_token ?
+                    <Modal_Services name="Servicio de Cirugía"/>
+                  :
+                    <div className="d-flex align-items-center justify-content-center">
+                      <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
+                        Agendar Servicio
+                      </button>               
+                    </div>
+                }
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
         <Row style={{marginTop: "50px", marginBottom: "80px"}}>
-        <Col md={4} xs={12}>
-        <Card style={{
-              height: "100%",
-              boxShadow:
-              hoveredCard === 3 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
-            }}
-            onMouseEnter={() => handleCardHover(3)}
-            onMouseLeave={handleCardLeave}
-          >
-            <Card.Body>
-              <img src={peluqueriaUrl}  style={{ display: 'block', margin: 'auto' }} width="120" height="150" alt="Foto de Consulta" />
-              <Card.Title>Peluquería</Card.Title>
-              <Card.Text>
-                Ofrecemos servicios de peluquería para todo tipo de mascotas.
-              </Card.Text>
-              <Card.Text>Algunos de nuestros servicios:</Card.Text>
-              <ul>
-                <li>Corte de pelo</li>
-                <li>Baño y secado</li>
-                <li>Corte de uñas</li>
-              </ul>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} xs={12}>
-        <Card style={{
-              height: "100%",
-              boxShadow:
-              hoveredCard === 4 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
-            }}
-            onMouseEnter={() => handleCardHover(4)}
-            onMouseLeave={handleCardLeave}
-          >
-          <Card.Body>
-            <img src={desparasitacionUrl}  style={{ display: 'block', margin: 'auto' }} width="120" height="150" alt="Foto de Consulta" />
-            <Card.Title>Desparasitación</Card.Title>
-            <Card.Text>
-              Ofrecemos tratamientos para desparasitar a tu mascota y prevenir enfermedades.
-            </Card.Text>
-            <Card.Text>Algunos de nuestros servicios:</Card.Text>
-            <ul>
-              <li>Desparasitación interna y externa</li>
-              <li>Prevención de enfermedades parasitarias</li>
-              <li>Asesoramiento sobre cuidados preventivos</li>
-            </ul>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col md={4} xs={12}>
-      <Card style={{
-              height: "100%",
-              boxShadow:
-              hoveredCard === 5 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
-            }}
-            onMouseEnter={() => handleCardHover(5)}
-            onMouseLeave={handleCardLeave}
-          >
-          <Card.Body>
-            <img src={emergenciaUrl}  style={{ display: 'block', margin: 'auto' }} width="120" height="150" alt="Foto de Consulta" />
-            <Card.Title>Emergencias</Card.Title>
-            <Card.Text>
-              Estamos disponibles las 24 horas del día para atender emergencias veterinarias.
-            </Card.Text>
-            <Card.Text>Algunos de nuestros servicios:</Card.Text>
-            <ul>
-              <li>Atención de urgencias</li>
-              <li>Estabilización de mascotas en estado crítico</li>
-              <li>Monitoreo y cuidados intensivos</li>
-            </ul>
-          </Card.Body>
-        </Card>
-      </Col>
-      </Row>
+          <Col md={4} xs={12}>
+            <Card style={{
+                height: "65vh",
+                boxShadow:
+                hoveredCard === 3 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
+              }}
+              onMouseEnter={() => handleCardHover(3)}
+              onMouseLeave={handleCardLeave}
+            >
+              <Card.Body>
+                <img src={peluqueriaUrl}  style={{ display: 'block', margin: 'auto' }} width="150" height="150" alt="Foto de Consulta" />
+                <Card.Title>Peluquería</Card.Title>
+                <Card.Text>
+                  Ofrecemos servicios de peluquería para todo tipo de mascotas.
+                </Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Corte de pelo</li>
+                  <li>Baño y secado</li>
+                  <li>Corte de uñas</li>
+                </ul>
+                {
+                  store.jwt_token ?
+                    <Modal_Services name="Servicio de Peluquería"/>
+                  :
+                    <div className="d-flex align-items-center justify-content-center">
+                      <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
+                        Agendar Servicio
+                      </button>               
+                    </div>
+                }
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} xs={12}>
+            <Card style={{
+                height: "65vh",
+                boxShadow:
+                hoveredCard === 4 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
+              }}
+              onMouseEnter={() => handleCardHover(4)}
+              onMouseLeave={handleCardLeave}
+            >
+              <Card.Body>
+                <img src={desparasitacionUrl}  style={{ display: 'block', margin: 'auto' }} width="150" height="150" alt="Foto de Consulta" />
+                <Card.Title>Desparasitación</Card.Title>
+                <Card.Text>
+                  Ofrecemos tratamientos para desparasitar a tu mascota y prevenir enfermedades.
+                </Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Desparasitación interna y externa</li>
+                  <li>Prevención de enfermedades parasitarias</li>
+                  <li>Asesoramiento y cuidados preventivos</li>
+                </ul>
+                {
+                    store.jwt_token ?
+                      <Modal_Services name="Servicio de Desparasitación"/>
+                    :
+                      <div className="d-flex align-items-center justify-content-center">
+                        <button onClick={() => {toast.error("Inicia sesión para agendar servicios")}} type="button" className="btn btn btn-secondary hover-effect mt-3">
+                          Agendar Servicio
+                        </button>               
+                      </div>
+                  }
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} xs={12}>
+            <Card style={{
+                height: "65vh",
+                boxShadow:
+                hoveredCard === 5 ? "0 0 10px rgba(103, 58, 183, 0.7)" : "",
+              }}
+              onMouseEnter={() => handleCardHover(5)}
+              onMouseLeave={handleCardLeave}
+            >
+              <Card.Body>
+                <img src={emergenciaUrl}  style={{ display: 'block', margin: 'auto' }} width="150" height="150" alt="Foto de Consulta" />
+                <Card.Title>Emergencias</Card.Title>
+                <Card.Text>
+                  Estamos disponibles las 24 horas del día para atender emergencias veterinarias.
+                </Card.Text>
+                <Card.Text>Algunos de nuestros servicios:</Card.Text>
+                <ul>
+                  <li>Atención de urgencias</li>
+                  <li>Estabilización de mascotas con cuadro clínico de estado crítico</li>
+                  <li>Monitoreo y cuidados intensivos</li>
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
     </Container>
     <Toaster/>
     </div>
