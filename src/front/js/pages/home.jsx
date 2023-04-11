05ea304122e8659df227a77ccc372252bcc267ce
Carlos Herrera <c.herrerav97@gmail.com>
2023-04-11 13:44:16 +0000

    Arreglo de estructura Componentes/Vistas

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import imgVet from "../../img/imgvet.jpg";
 import imgCatDog from "../../img/imgcatdog.jpg";
 import imgsec1left from "../../img/imgsec1left.png";
@@ -13,162 +13,251 @@ import emergenciaUrl from "../../img/emergencia.png";
 import peluqueriaUrl from "../../img/peluqueria.png";
 import vacunacionUrl from "../../img/vacunacion.png";
 import mascotas3 from "../../img/mascotas3.jpg";
-import services from "../component/services.jsx";
+import services from "./services.jsx";
 import about from "../pages/about.jsx";
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
-</style>
-
+  @import
+  url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
+</style>;
 
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
+  return (
+    <div className="text-center">
+      <div
+        className="jumbotron ms-2 pt-5 mt-3 "
+        style={{ background: "white", fontFamily: "Montserrat, sans-serif" }}
+      >
+        <div>
+          <div className="row d-flex mt-5 mb-5">
+            <div className="col-sm-4 ">
+              <img style={{ width: "25em" }} src={imgsec1left} />
+            </div>
+            <div className="col-sm-4 ">
+              <h4 className="mt-2 pt-2">
+                {" "}
+                <strong>
+                  {" "}
+                  El mejor equipo del mundo en <br></br> servicios de cuidado
+                  para mascotas <br></br> <br></br>{" "}
+                </strong>{" "}
+              </h4>
 
-		<div className="text-center" >
-			<div className="jumbotron ms-2 pt-5 mt-3 " style={{background:"white", fontFamily: "Montserrat, sans-serif"}}>
+              <p
+                className=" lh-base text-center justify-content"
+                style={{ align: "justify" }}
+              >
+                En la clínica veterinaria Huellas PetCare, su mascota es nuestra
+                prioridad. Estamos orgullosos de ofrecer nuestros servicios
+                veterinarios a gatos y perros.
+              </p>
+              <Link to="/services">
+                <span
+                  className="btn btn-primary btn-lg rounded-pill justify-content-center "
+                  style={{ backgroundColor: "#FF6FB5" }}
+                  href={"Servicios"}
+                  role="button"
+                >
+                  Nuestros Servicios
+                </span>
+              </Link>
+            </div>
+            <div className="col-sm-4 ">
+              <img style={{ width: "23em" }} src={girldog} />
+            </div>
+          </div>
+        </div>
+      </div>
 
-				<div>
-					<div className="row d-flex mt-5 mb-5">
-						<div className="col-sm-4 ">
-							<img style={{width:"25em"}} src= {imgsec1left}/>
-						</div>
-						<div className="col-sm-4 ">
-							<h4  className="mt-2 pt-2" > <strong> El  mejor  equipo  del  mundo en <br></br> servicios de cuidado para mascotas <br></br> <br></br> </strong> </h4>
-					
-							<p className=" lh-base text-center justify-content" style={{align:"justify"}}>En la clínica veterinaria Huellas PetCare, su mascota es nuestra prioridad. Estamos orgullosos de ofrecer nuestros servicios veterinarios a gatos y perros.</p>
-							<Link to="/services">
-								<span className="btn btn-primary btn-lg rounded-pill justify-content-center " style={{backgroundColor:"#FF6FB5"}} href={"Servicios"} role="button"
-								>Nuestros Servicios</span>
-							</Link>
+      <div
+        className="jumbotron p-5 mt-3 mb-3"
+        style={{ backgroundColor: "#AB46D2" }}
+      >
+        <div className="card mb-3" style={{ maxWwidth: "540px" }}>
+          <div className="row g-0">
+            <div
+              className="col-md-8 mb-3"
+              style={{ fontFamily: "Montserrat, sans-serif" }}
+            >
+              <div className="card-body">
+                <h3 className="card-title fw-bold pt-3 mt-3">
+                  {" "}
+                  Huellas PetCare <br></br> <br></br>
+                </h3>
+                <p className="card-text">
+                  En nuestra clínica, la atención veterinaria excepcional y el
+                  servicio al cliente es nuestra forma de vida. No solo
+                  destacamos por nuestro amor a los animales, sino también por
+                  nuestros amplios años de experiencia. En la actualidad estamos
+                  innovando con nuestro proyecto tecnológico de incorporación de
+                  la aplicación <u> Huellas PetCare</u> a nuestro website.{" "}
+                </p>
+                <p className="card-text">
+                  <small className="text-muted"></small>
+                </p>
+                <Link to="/about">
+                  <span
+                    className="btn btn-primary btn-lg rounded-pill justify-content-center "
+                    style={{ backgroundColor: "#FF6FB5" }}
+                    href={"About"}
+                    role="button"
+                  >
+                    Conocer más
+                  </span>
+                </Link>
+              </div>
+            </div>
 
-						</div>
-						<div className="col-sm-4 ">
-							<img style={{width:"23em"}} src= {girldog}/>
-						</div>
-					</div>
-				</div>
-	
-			</div> 
+            <div className="col-md-4">
+              <img
+                src={teamvet2}
+                className="img-fluid rounded-startgit status pt-3 pe-3"
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div className="jumbotron p-5 mt-3 mb-3" style={{backgroundColor:"#AB46D2"}}>
-				<div className="card mb-3" style={{maxWwidth: "540px"}}>
-					<div className="row g-0">
-						
-						<div className="col-md-8 mb-3" style={{fontFamily: "Montserrat, sans-serif"}}>
-							<div className="card-body">
-								<h3 className="card-title fw-bold pt-3 mt-3"> Huellas PetCare <br></br> <br></br></h3>
-								<p className="card-text">En nuestra clínica, la atención veterinaria excepcional y el servicio al cliente es nuestra forma de vida.  No solo destacamos por nuestro amor a los animales, sino también por nuestros amplios años de experiencia.  En la actualidad estamos innovando con nuestro proyecto tecnológico de incorporación de la aplicación <u> Huellas PetCare</u> a nuestro website. </p>
-								<p className="card-text"><small className="text-muted"></small></p>
-								<Link to="/about">
-									<span className="btn btn-primary btn-lg rounded-pill justify-content-center " style={{backgroundColor:"#FF6FB5"}} href={"About"} role="button"
-									>Conocer más</span>
-								</Link>
-							</div>
-						</div>
+      <div className="jumbotron p-2 mt-3" style={{ background: "white" }}>
+        <h1 className="display-4">
+          {" "}
+          <strong> Adquiere nuestros Servicios </strong>
+        </h1>
+        <p className="lead"></p>
+        <div>
+          <div className="row d-flex gap-2 justify-content-center mt-5 mb-5">
+            <div className="col-sm-2 ">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src={consultaUrl}
+                    style={{ display: "block", margin: "auto" }}
+                    width="120"
+                    height="150"
+                    alt="Foto de Consulta"
+                  />
+                  <h5 className="card-title">Consultas</h5>
+                  <p className="card-text"></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-2 ">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src={cirugiaUrl}
+                    style={{ display: "block", margin: "auto" }}
+                    width="120"
+                    height="150"
+                    alt="Foto de Consulta"
+                  />
+                  <h5 className="card-title">Cirugía</h5>
+                  <p className="card-text"></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-2 ">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src={peluqueriaUrl}
+                    style={{ display: "block", margin: "auto" }}
+                    width="120"
+                    height="150"
+                    alt="Foto de Consulta"
+                  />
+                  <h5 className="card-title">Peluquería</h5>
+                  <p className="card-text"></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-2 ">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src={vacunacionUrl}
+                    style={{
+                      display: "block",
+                      margin: "auto",
+                      marginTop: "20px",
+                      marginBottom: "25px",
+                    }}
+                    width="100"
+                    height="100"
+                    alt="Foto de Consulta"
+                  />
+                  <h5 className="card-title">Vacunación</h5>
+                  <p className="card-text"></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-2 ">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src={emergenciaUrl}
+                    style={{ display: "block", margin: "auto" }}
+                    width="120"
+                    height="150"
+                    alt="Foto de Consulta"
+                  />
+                  <h5 className="card-title">Emergencias</h5>
+                  <p className="card-text"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link to="/signup">
+          <span
+            className="btn btn-primary btn-lg rounded-pill justify-content-center "
+            style={{ backgroundColor: "#FF6FB5" }}
+            href={"About"}
+            role="button"
+          >
+            Regístrate
+          </span>
+        </Link>
+      </div>
 
-						<div className="col-md-4">
-							<img src={teamvet2} className="img-fluid rounded-startgit status pt-3 pe-3" alt="..."/>
-						</div>	
-
-					</div>
-				</div>
-			</div>
-
-
-			<div className="jumbotron p-2 mt-3" style={{background:"white"}}>
-				<h1 className="display-4"> <strong> Adquiere nuestros Servicios </strong></h1>
-				<p className="lead"></p>
-				<div>
-				<div className="row d-flex gap-2 justify-content-center mt-5 mb-5" >
-					<div className="col-sm-2 ">
-						<div className="card">
-							<div className="card-body">
-								<img
-									src={consultaUrl}
-									style={{ display: "block", margin: "auto" }}
-									width="120"
-									height="150"
-									alt="Foto de Consulta"
-								/>
-								<h5 className="card-title">Consultas</h5>
-								<p className="card-text"></p>
-							</div>
-						</div>
-					</div>
-					<div className="col-sm-2 ">
-						<div className="card">
-							<div className="card-body">
-								<img
-									src={cirugiaUrl}
-									style={{ display: "block", margin: "auto" }}
-									width="120"
-									height="150"
-									alt="Foto de Consulta"
-								/>
-								<h5 className="card-title">Cirugía</h5>
-								<p className="card-text"></p>
-							</div>
-						</div>
-					</div>
-					<div className="col-sm-2 ">
-						<div className="card">
-							<div className="card-body">
-								<img src={peluqueriaUrl}  style={{ display: 'block', margin: 'auto' }} width="120" height="150" alt="Foto de Consulta" />
- 								<h5 className="card-title">Peluquería</h5>
-								<p className="card-text"></p>
-							</div>
-						</div>
-					</div>
-					<div className="col-sm-2 ">
-						<div className="card">
-							<div className="card-body">
-							<img
-								src={vacunacionUrl}
-								style={{ display: "block", margin: "auto", marginTop: "20px", marginBottom: "25px" }}
-								width="100"
-								height="100"
-								alt="Foto de Consulta"
-							/>
- 								<h5 className="card-title">Vacunación</h5>
-								<p className="card-text"></p>
-							</div>
-						</div>
-					</div>
-					<div className="col-sm-2 ">
-						<div className="card">
-							<div className="card-body">
-								<img src={emergenciaUrl}  style={{ display: 'block', margin: 'auto' }} width="120" height="150" alt="Foto de Consulta" />
-								<h5 className="card-title">Emergencias</h5>
-								<p className="card-text"></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-				<Link to="/signup">
-					<span className="btn btn-primary btn-lg rounded-pill justify-content-center " style={{backgroundColor:"#FF6FB5"}} href={"About"} role="button"
-					>Regístrate</span>
-				</Link>
-			</div>
-
-			<div>
-				<div id="carouselExampleSlidesOnly" className="carousel slide mb-3 pb-3" data-bs-ride="carousel">
-					<div className="carousel-inner">
-						<div className="carousel-item active">
-								<img src={mascotas3} className="d-block w-100" style={{background:"white", height: "50vh" }} alt="..."/>
-								<div className="carousel-caption position-absolute top-0 start-0 end-0">
-									<h1 className="p-0 mb-2 display-4 p-3 mb-5 bg-dark text-light fs-1 fw-bold" style={{opacity:"0.15" }}>Porque tu mascota se lo merece </h1>
-									<h2  className="p-1 mb-0 mt-5 display-4 bg-dark text-light fw-bold align-bottom" style={{opacity:"0.25" }}>Simplemente te ayudamos a cuidarlos</h2>
-								</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		<Toaster />
-		</div>
-	);
+      <div>
+        <div
+          id="carouselExampleSlidesOnly"
+          className="carousel slide mb-3 pb-3"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src={mascotas3}
+                className="d-block w-100"
+                style={{ background: "white", height: "50vh" }}
+                alt="..."
+              />
+              <div className="carousel-caption position-absolute top-0 start-0 end-0">
+                <h1
+                  className="p-0 mb-2 display-4 p-3 mb-5 bg-dark text-light fs-1 fw-bold"
+                  style={{ opacity: "0.15" }}
+                >
+                  Porque tu mascota se lo merece{" "}
+                </h1>
+                <h2
+                  className="p-1 mb-0 mt-5 display-4 bg-dark text-light fw-bold align-bottom"
+                  style={{ opacity: "0.25" }}
+                >
+                  Simplemente te ayudamos a cuidarlos
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
 };
---
+++ src/front/js/pages/myAccount.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Pets } from "../component/myPets.jsx";
+import { My_Services } from "../component/myServices.jsx";
+import { Context } from "../store/appContext.js";
+import toast, { Toaster } from "react-hot-toast";
+import fondologo from "../../img/fondologo.png";
+
+export const My_Account = () => {
+  const { store, actions } = useContext(Context);
+  const [animation, setAnimation] = useState(0);
+  const [view, setView] = useState(0);
+
+  useEffect(() => {
+    actions.getProfile();
+    setAnimation(0);
+  }, [store.jwt_token]);
+
+  const LoadPage = (num) => {
+    setView(num);
+    setAnimation(1);
+  };
+
+  const estilo = {
+    backgroundImage: `url(${fondologo})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: `center`,
+  };
+
+  return (
+    <div className="my-account-css">
+      <div className="d-flex m-5 justify-content-between h-100 ">
+        <div className="border col-3 d-flex flex-column me-5 fondo">
+          <div className="d-flex align-items-center justify-content-center flex-column">
+            <h5 className="text-light pt-5">INFORMACIÓN DE USUARIO</h5>
+            {store.user ? (
+              <div className="d-flex align-items-center justify-content-center flex-column p-3">
+                <img
+                  className="profile-img pb-2"
+                  src="https://cdn-icons-png.flaticon.com/512/3076/3076404.png"
+                  alt="IMG Profile"
+                />
+                <h6 className="text-light p-2">
+                  Nombre: {store.user.name} {store.user.last_name}
+                </h6>
+                <h6 className="text-light p-2">Email: {store.user.email}</h6>
+                <h6 className="text-light p-2">
+                  Teléfono: {store.user.phone_number}
+                </h6>
+              </div>
+            ) : (
+              <div
+                className="d-flex align-items-center justify-content-center fs-3"
+                style={{ height: "45vh" }}
+              >
+                <div className="spinner-border text-light" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="d-flex align-items-center justify-content-center p-3">
+            <button
+              onClick={(e) => LoadPage(1)}
+              className="btn btn-light hover-nav"
+              style={{ color: "#AB46D2", fontWeight: "bold" }}
+            >
+              MIS MASCOTAS{" "}
+            </button>
+          </div>
+          <div className="d-flex align-items-center justify-content-center p-3">
+            <button
+              onClick={(e) => LoadPage(2)}
+              className="btn btn-light hover-nav"
+              style={{ color: "#AB46D2", fontWeight: "bold" }}
+            >
+              SERVICIOS AGENDADOS
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={`flex-fill slide-in-right ${
+            animation == 1 ? "slide-in-right" : "nada"
+          } borde `}
+          style={estilo}
+        >
+          {view == 0 ? <p>elem 2 </p> : view == 1 ? <Pets /> : <My_Services />}
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
