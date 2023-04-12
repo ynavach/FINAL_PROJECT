79b6fc27550164e702878673af508605bd50da58
pulgazion <reimarrodri@gmail.com>
2023-04-12 21:37:36 +0000

    Cambios en el About Us

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "../../styles/home.css";
 
 export const Footer = () => (
-	<footer className="footer container-fluid py-3 d-flex align-items-center justify-content-center sticky-bottom" style={{minHeight:"5vh", backgroundColor:"#AB46D2"}}>
-		<p className="d-flex align-items-center justify-content-center text-white m-0 me-3">Copyright © 2023 Huellas PetCare Website</p>
-		<div className="social-links">
-			<i className= "fa-brands fa-facebook me-2 text-light  fa-2x fa-lg"></i>
-			<i className= "fa-brands fa-instagram me-2 text-light  fa-2x fa-lg"></i>
-			<i className= "fa-brands fa-linkedin me-5 pe-5 text-light fa-2x fa-lg"></i>
-		</div>
-    </footer>
-	);
+	<footer className="footer container-fluid py-3 d-flex align-items-center justify-content-center" style={{minHeight:"5vh", backgroundColor:"#AB46D2",  bottom: 0, width: "100%"}}>
+	  <p className="d-flex align-items-center justify-content-center text-white m-0 me-3">Copyright © 2023 Huellas PetCare Website
+	  </p>
+	  <div className="social-links">
+		<i className= "fa-brands fa-facebook me-2 text-light  fa-2x fa-lg"></i>
+		<i className= "fa-brands fa-instagram me-2 text-light  fa-2x fa-lg"></i>
+		<i className= "fa-brands fa-linkedin me-5 pe-5 text-light fa-2x fa-lg"></i>
+	  </div>
+	</footer>
+  );
+  
--- src/front/js/pages/about.jsx
+++ src/front/js/pages/about.jsx
@@ -1,191 +1,144 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
-import { Context } from "../store/appContext";
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import toast, { Toaster } from 'react-hot-toast';
 
-import avatargenerico1 from "../../img/avatargenerico1.png"; 
+import { Context } from "../store/appContext";
+
+import carlos1Url from "../../img/carlos1.png";
+import reimarUrl from "../../img/Reimar.png";
+import wilfredoUrl from "../../img/wilfredo.png";
+import fondoquienesUrl from "../../img/fondoquienes.png";
+import gatodevUrl from "../../img/gatodev.png";
 
 export const About = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="container">
-
-			<div className="shadow-lg  p-3 mb-5 mt-5 bg-body rounded d-flex justify-content-center " role="alert">
-				<table style={{height: "100px"}}>
-					<tbody>
-						<tr>
-							<td className="fs-1">Quienes Somos</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-
-			<div>
-				<nav>
-					<div className="nav nav-tabs"  id="nav-tab" role="tablist">
-						<button className="nav-link active fs-3" style={{color:"purple"}} id="nav-presentacion-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Bienvenidos</button>
-						<button className="nav-link fs-3" style={{color:"purple"}} id="nav-informacion-tab" data-bs-toggle="tab" data-bs-target="#nav-informacion" type="button" role="tab" aria-controls="nav-informacion" aria-selected="false">Nuestro Proyecto</button>
-						<button className="nav-link fs-3" style={{color:"purple"}} id="nav-proceso-tab" data-bs-toggle="tab" data-bs-target="#nav-proceso" type="button" role="tab" aria-controls="nav-proceso" aria-selected="false">Desarrollo</button>
-						<button className="nav-link fs-3" style={{color:"purple"}} id="nav-Lenguajes-tab" data-bs-toggle="tab" data-bs-target="#nav-lenguajes" type="button" role="tab" aria-controls="nav-lenguajes" aria-selected="false">Herramientas</button>
-						<button className="nav-link fs-3" style={{color:"purple"}} id="nav-huellas-tab" data-bs-toggle="tab" data-bs-target="#nav-huellas" type="button" role="tab" aria-controls="nav-huellas" aria-selected="false">Huellas PetCare</button>
-					</div>
-				</nav>
-				<div className="tab-content" id="nav-tabContent">
-					<div className="tab-pane fade show active  mt-3 pt-3" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-						<h5 className="fs-4">¡Buenas Tardes!</h5>
-						<p  className="mb-3 pb-3 fs-4">Somos los integrantes del equipo de desarrollo de la aplicación Huellas PetCare: </p>
-
-					<div className="d-flex justify-content-center mb-3">
-						<div className="row row-cols-1 row-cols-md-4 g-4" style={{maxWidth: "900px"}}>
-							<div className="col">
-								<div className="card">
-									<img src="avatargenerico1.png" className="card-img-top" alt="..."/>
-									<div className="card-body">
-										<h5 className="card-title">Wilfredo Andrade</h5>
-										<div className="d-flex justify-content-center " >
-										<i className= "fa-brands fa-github pe-1"></i>
-										<p className="card-text">github.com/N1kf3</p>
-										</div>								
-
-									</div>
-								</div>
-							</div>
-							<div className="col">
-								<div className="card">
-									<img src="avatargenerico1.png" className="card-img-top" alt="..."/>
-									<div className="card-body">
-										<h5 className="card-title">Carlos Herrera</h5>
-										<div className="d-flex justify-content-center " >
-										<i className= "fa-brands fa-github pe-1"></i>
-										<p className="card-text">github.com/careduh97</p>
-										</div>	
-									</div>
-								</div>
-							</div>
-							<div className="col">
-								<div className="card">
-									<img src="avatargenerico1.png" className="card-img-top" alt="..."/>
-									<div className="card-body">
-										<h5 className="card-title">Reimar Rodríguez</h5>
-										<div className="d-flex justify-content-center " >
-										<i className= "fa-brands fa-github pe-1"></i>
-										<p className="card-text">github.com/pulgazion</p>
-										</div>	
-									</div>
-								</div>
-							</div>
-							<div className="col">
-								<div className="card">
-									<img src="avatargenerico1.png" className="card-img-top" alt="..."/>
-									<div className="card-body">
-										<h5 className="card-title">Yunensey Nava</h5>
-										<div className="d-flex justify-content-center " >
-										<i className= "fa-brands fa-github pe-1"></i>
-										<p className="card-text">github.com/ynavach</p>
-										</div>	
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-					<div className="tab-pane fade fs-4" id="nav-informacion" role="tabpanel" aria-labelledby="nav-informacion-tab">
-						<div  className="card text-center">
-							<div  className="card-header fs-4">
-								<br></br>
-							</div>
-							<div  className="card-body">
-								<h5  className="card-title"></h5>
-								<p  className="card-text"></p>
-								<div className=" mb-3 pb-3" >
-									<p className="fw-bold mt-1 pt-1 text-decoration-underline">Descripción del proyecto: <br></br> </p>
-									<p>Aplicación web para el control de mascotas.</p>
-									<p className="fw-bold text-decoration-underline">Nuestro público: <br></br>	</p>
-									<p>	La aplicación va dirigida a todos nuestros clientes que deseen gestionar los servicios veterinarios que ofrece la clínica a través de la web.</p>
-									<p className="fw-bold text-decoration-underline">Objetivo: <br></br></p>
-									<p>Nuestro objetivo se enfoca en ofrecer una experiencia de uso sencilla en la que el cliente simplemente tenga que registrarse y entrar en su cuenta para comenzar a guardar la información de sus mascotas y agendar servicios de la clínica veterinaria.</p>
-								</div>	
-							</div>
-							<div  className="card-footer text-muted">
-							<br></br>
-							</div>
-						</div>
-					</div>
-				
-					<div className="tab-pane fade" id="nav-proceso" role="tabpanel" aria-labelledby="nav-proceso-tab">
-				
-						<div  className="card text-center fs-4">
-							<div  className="card-header fs-4">
-								<br></br>
-							</div>
-							<div  className="card-body">
-								<h5  className="card-title"></h5>
-								<p  className="card-text"></p>
-								<div className=" mb-3 pb-3" >
-									<p className="fw-bold mt-1 pt-1"></p>
-									<p>Nuestro proceso de creación de la Aplicación estuvo basado en la metodología ágil Scrum.</p>
-									<p className="">La Paleta de Colores escogida estuvo basada en colores vibrantes.  <br></br>	</p>
-									<p>La tipografía utilizada fue Montserrat de GoogleFonts. </p>
-									<p className="">El logotipo fue pensado en combinación del nombre con el propósito clínico, haciéndolo así, de fácil entendimiento.<br></br></p>
-									<p></p>
-								</div>	
-							</div>
-							<div  className="card-footer text-muted">
-							<br></br>
-							</div>
-						</div>
-
-					</div>
-
-					<div className="tab-pane fade mb-3 pb-3 fs-4" id="nav-lenguajes" role="tabpanel" aria-labelledby="nav-lenguajes-tab">
-						
-						<div  className="card text-center">
-							<div  className="card-header fs-4">
-								Lenguajes y Tecnologías
-							</div>
-							<div  className="card-body">
-								<h5  className="card-title pb-2 mb-2">Modales React -      Flask -     Postgres <br></br> </h5>
-								<p  className="card-text pb-4 mb-4">SQLAlchemy     -     JWT    -     Cloudinary   </p>
-								<div className="d-flex justify-content-between mb-3 pb-3" >
-									<i className= "fa-brands fa-html5 fa-64x fa-lg ms-4 ps-4"></i>
-									<i className= "fa-brands fa-react fa-64x fa-lg"></i>
-									<i className= "fa-brands fa-python fa-64x fa-lg"></i>
-									<i className= "fa-brands fa-square-github fa-64x fa-lg"></i>
-									<i className= "fa-brands fa-js fa-64x fa-lg"></i>
-									<i className= "fa-brands fa-css3-alt fa-64x fa-lg"></i>
-									<i className= "fa-brands fa-bootstrap fa-64x fa-lg me-4 pe-4"></i>
-								</div>	
-							</div>
-							<div  className="card-footer text-muted">
-								Full Stack Developers - FrontEnd - BackEnd
-							</div>
-						</div>
-					</div>
-
-					<div className="tab-pane fade" id="nav-huellas" role="tabpanel" aria-labelledby="nav-huellas-tab">
-												
-						<div className="card mb-3" >
-							<div className="row g-0">
-								<div className="col-md-4">
-									<img src="avatargenerico1.png" className="img-fluid rounded-start" alt="..."/>
-								</div>
-								<div className="col-md-8 d-flex justify-content-center mt-3 pt-3">
-									<div className="card-body">
-										<h3 className="card-title">¡Gracias por su atención!</h3>
-										<p className="card-text mt-3 pt-3">Proyecto Final 4Geeks Academy Caracas.</p>
-										<p className="card-text"><small className="text-muted mt-3 pt-3">Huellas PetCare © 2023</small></p>
-									</div> 
-								</div>
-							</div>
-						</div>	
-						
-					</div>
-				</div>
-			</div>		
-				
-		</div>
-	);
+  return (
+    <div style={{ position: "relative" }}>
+      <Container style={{ marginTop: "90px" }}>
+        <Row>
+          <Col md={6}>
+          <h1 className="mt-5" style={{ textAlign: 'center', fontFamily: "cursive" }}>Quiénes Somos</h1>
+            <div className="fusion-column col-lg-12 col-md-12 col-sm-12" style={{ marginTop: "10px" }}>
+              <img
+                src={fondoquienesUrl}
+                alt="Titulo Quiénes Somos"
+                style={{ height: "400px", width: "100%", position: "relative"}}
+              />
+            </div>
+          </Col>
+          <Col md={6}>
+            <div style={{ position: "absolute", top: "150px", textAlign: 'center', paddingRight: '30px' }}>
+              <h6 className="my-3"style={{ lineHeight: '1.5' }}>Huellas Petcare nació de la necesidad del mercado de una herramienta que hiciera que el servicio veterinario fuese más fácil y accesible. Sabemos que para muchos dueños de mascotas, programar citas y servicios en la clínica veterinaria puede ser un proceso complicado y estresante. Por eso, decidimos crear una aplicación web que hiciera este proceso más fácil y sin estrés.</h6>
+              <h6 className="my-3" style={{ lineHeight: '1.5' }}>En nuestra plataforma los usuarios pueden programar citas y servicios en cualquier momento y lugar. Sabemos que cada animal es único y requiere una atención específica. Así que como desarrolladores, nos enfocamos en crear una plataforma que hiciera posible esto, sin descuidar la calidad y la atención personalizada.</h6>
+            </div> 
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <div className="fusion-column col-lg-12 col-md-12 col-sm-12" style={{ marginTop: "150px", textAlign: 'left', paddingRight: '30px' }}>
+              <h6 className="my-3"style={{ lineHeight: '1.5' }}>
+              En Huellas Petcare, estamos comprometidos a seguir mejorando nuestra aplicación para brindar la mejor experiencia posible a nuestros usuarios. Esto nos lleva a implementar algunas mejoras en el futuro:
+              </h6>
+              <ul>
+                <li className="my-3"style={{ textAlign: "left" , lineHeight: '1.5' }}>Permitir que los médicos veterinarios deshabiliten temporalmente servicios que no pueden brindar, para una mejor atención a las mascotas.</li>
+                <li className="my-3"style={{ textAlign: 'left', lineHeight: '1.5' }}>Dar la opción a los usuarios de modificar los datos de su perfil, para mayor flexibilidad y comodidad</li>
+                <li className="my-3"style={{ textAlign: 'left', lineHeight: '1.5' }}>Facilitar a los médicos la visualización de la lista de usuarios y mascotas registradas, para un mejor seguimiento y atención.</li>
+                <li className="my-3"style={{ textAlign: 'left', lineHeight: '1.5' }}>Permitir que los médicos editen el historial médico de las mascotas, para llevar un registro completo y detallado.</li>
+                <li className="my-3"style={{ textAlign: 'left', lineHeight: '1.5' }}>Mostrar el historial de servicios en la vista de las mascotas, para una mejor visualización y seguimiento.</li>
+              </ul>
+            </div>
+          </Col>
+          <Col md={6}>
+          <h1 className="mt-5" style={{ textAlign: 'center', marginLeft: "80px" , fontFamily: "cursive" }}>Desarrollo a futuro</h1>
+            <img
+              src={gatodevUrl}
+              alt="Fondo de servicios"
+              style={{ width: "120%", position: "relative" }}
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+        <Col md={1} style={{ fontSize: "20px", marginRight: "50px", marginBottom: "100px" }}>
+      </Col>
+        </Row>
+  
+      </Container>
+      <div style={{ backgroundColor: 'rgba(241, 241, 241, 2)', width: '100%', height: '650px', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '30px' }}>
+        <h1 style={{ textAlign: 'center', marginBottom: "10px", fontFamily: "cursive" }}>Tecnologías</h1>
+        <h6 className="my-4" style={{ maxWidth: "2000px", margin: "auto", textAlign: 'center', marginBottom: "20px", lineHeight: '1.5' }}>La plataforma se apoya en diversas tecnologías para su desarrollo. En el frontend, se utilizan herramientas de marcado y diseño como HTML5, CSS3 y Bootstrap para lograr una interfaz de usuario atractiva y amigable. En el backend, se utilizan lenguajes de programación como JavaScript, React, Python y Flask, y se emplea una base de datos para almacenar y recuperar información relevante. </h6>
+        <h6 className="my-4" style={{ maxWidth: "2000px", margin: "auto", textAlign: 'center', marginBottom: "20px" , lineHeight: '1.5' }}>Además de las tecnologías mencionadas, también se utilizan APIs de terceros para mejorar la funcionalidad de la plataforma. En particular, se utilizan las APIs de Cloudinary y Hot Toast. La primera se utiliza para la gestión de imágenes, mientras que la segunda se utiliza para la gestión de notificaciones y mensajes push. Todo el trabajo en equipo y control de versiones se realiza a través de Git. Gracias a estas tecnologías, la plataforma ofrece servicio efectivo y confiable.</h6>
+    
+        <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: "30px", marginTop: "10px" }}>
+          <i className="fab fa-html5 fa-4x ms-4 m-4"></i>
+          <i className="fab fa-react fa-4x mx-2 m-4"></i>
+          <i className="fab fa-python fa-4x mx-2 m-2"></i>
+          <i className="fab fa-github-square fa-4x mx-2 m-4"></i>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <i className="fab fa-js fa-4x mx-2"></i>
+          <i className="fab fa-css3-alt fa-4x mx-2"></i>
+          <i className="fab fa-bootstrap fa-4x me-2"></i>
+          <i className="fas fa-database fa-4x mx-2"></i>
+          <i className="fab fa-git-alt fa-4x mx-2"></i>
+        </div>
+      </div>
+      <section className="team text-center py-5">
+      <div className="container">
+        <div className="header my-5">
+          <h1 style={{ fontFamily: "cursive" }}> Nuestro equipo</h1>
+          <h5 style={{ margin: "auto", textAlign: 'center' }}>Huellas Petcare</h5>
+        </div>
+        <div className="row">
+          <div className="col-md-6 col-lg-3">
+            <div className="img-block mb-5">
+              <img src={carlos1Url} className="img-fluid img-thumbnail rounded-circle" alt="image1" />
+              <div className="content mt-2">
+                <h4>Carlos Herrera</h4>
+                <a href="https://github.com/careduh97" target="_blank">
+                  <i class="fab fa-github pe-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 ">
+            <div className="img-block mb-5">
+              <img src={reimarUrl} className="img-fluid img-thumbnail rounded-circle" alt="image1" />
+              <div className="content mt-2">
+                <h4>Reimar Rodriguez</h4>
+                <a href="https://github.com/pulgazion" target="_blank">
+                  <i class="fab fa-github pe-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3">
+            <div className="img-block mb-5">
+              <img src={wilfredoUrl} className="img-fluid img-thumbnail rounded-circle" alt="image1" />
+              <div className="content mt-2">
+                <h4>Wilfredo Andrade</h4>
+                <a href="https://github.com/N1kf3" target="_blank">
+                  <i class="fab fa-github pe-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3">
+            <div className="img-block mb-5">
+              <img src={reimarUrl} className="img-fluid img-thumbnail rounded-circle" alt="image1" />
+              <div className="content mt-2">
+                <h4>Yunensey Nava</h4>
+                <a href="https://github.com/ynavach" target="_blank">
+                  <i class="fab fa-github pe-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+  );  
 };
+
